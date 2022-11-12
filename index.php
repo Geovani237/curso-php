1acfb013c10b30ba3dcf52e9eb1172706f5d1678
Geovani carlos de souza <94084031+Geovani237@users.noreply.github.com>
2022-11-11 21:56:36 -0300

    definição de variáveis

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php 
-echo 'Olá mundo';
+echo 'Olá mundo<br>';
+echo 'Geovani Carlos de Souza';
+// http://localhost/curso-php/index.php
+
+print('<h1>escrevendo</h1>');
+
+print_r('<h2>Escrevendo de novo</h2>');
+
+$nome = 'Geovani';
+$idade = 18;
+$altura = 1.75;
+$fumante = true;
+
+
+echo '<br>';
+echo $nome . '<br>';
+echo $idade . '<br>';
+echo $altura . '<br>';
+echo $fumante . '<br>';
+echo 'Olá ' . $nome . ' você tem ' . $idade . ' anos';
 ?>
